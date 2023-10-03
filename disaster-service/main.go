f65272ebfdcdc8b3d028709db808d199a2e59abf
Salbert <numeoriginaldemail@gmail.com>
2023-10-03 14:25:28 +0300

    Checkpoint 1 - Issues fixed.

--- disaster-service/main.go
+++ disaster-service/main.go
@@ -1,1 +1,82 @@
-package disaster_service
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	//GET /disasters
+	router.HandleFunc("/disasters", GetDisasters).Methods("GET")
+
+	//GET /disasters/list?location={location}&active={active}
+	router.HandleFunc("/disasters/list", GetDisasterList).Methods("GET").Queries("location", "{location}", "active", "{active}")
+
+	//POST /disasters/alert?location={location}
+	router.HandleFunc("/disasters/alert", PostDisasterAlert).Methods("POST").Queries("location", "{location}")
+
+	//PUT /disasters/alert/{alert_id}
+	router.HandleFunc("/disasters/alert/{alert_id}", PutDisasterAlert).Methods("PUT")
+
+	//DELETE /disasters/alert/{alert_id}
+	router.HandleFunc("/disasters/alert/{alert_id}", DeleteDisasterAlert).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8001", router))
+}
+
+func GetDisasters(w http.ResponseWriter, _ *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(`{"message": "GetDisasters called"}`))
+	if err != nil {
+		return
+	}
+}
+
+func GetDisasterList(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	location := query.Get("location")
+	active := query.Get("active")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(`{"message": "GetDisasterList called with location=` + location + ` and active=` + active + `"}`))
+	if err != nil {
+		return
+	}
+}
+
+func PostDisasterAlert(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	location := query.Get("location")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(`{"message": "PostDisasterAlert called with location=` + location + `"}`))
+	if err != nil {
+		return
+	}
+}
+
+func PutDisasterAlert(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	alertId := vars["alert_id"]
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(`{"message": "PutDisasterAlert called with alert_id=` + alertId + `"}`))
+	if err != nil {
+		return
+	}
+}
+
+func DeleteDisasterAlert(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	alertId := vars["alert_id"]
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(`{"message": "DeleteDisasterAlert called with alert_id=` + alertId + `"}`))
+	if err != nil {
+		return
+	}
+}
--- weather-service/main.go
+++ weather-service/main.go
@@ -1,1 +1,82 @@
-package weather_service
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+
+	//GET /weather/locations?country={country}
+	router.HandleFunc("/weather/locations", GetLocations).Methods("GET").Queries("country", "{country}")
+
+	//GET /weather/current?location={location}
+	router.HandleFunc("/weather/current", GetCurrentWeather).Methods("GET").Queries("location", "{location}")
+
+	//GET /weather/forecast?location={location}
+	router.HandleFunc("/weather/forecast", GetWeatherForecast).Methods("GET").Queries("location", "{location}")
+
+	//POST /weather/add_data?type={type}
+	router.HandleFunc("/weather/add_data", AddWeatherData).Methods("POST").Queries("type", "{type}")
+
+	//DELETE /weather/delete_data?type={type}
+	router.HandleFunc("/weather/delete_data", DeleteWeatherData).Methods("DELETE").Queries("type", "{type}")
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
+func GetLocations(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	country := query.Get("country")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(`{"message": "GetLocations called with country=` + country + `"}`))
+	if err != nil {
+		return
+	}
+}
+
+func GetCurrentWeather(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	location := query.Get("location")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(`{"message": "GetCurrentWeather called with location=` + location + `"}`))
+	if err != nil {
+		return
+	}
+}
+
+func GetWeatherForecast(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	location := query.Get("location")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(`{"message": "GetWeatherForecast called with location=` + location + `"}`))
+	if err != nil {
+		return
+	}
+}
+
+func AddWeatherData(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	dataType := query.Get("type")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(`{"message": "AddWeatherData called with type=` + dataType + `"}`))
+	if err != nil {
+		return
+	}
+}
+
+func DeleteWeatherData(w http.ResponseWriter, r *http.Request) {
+	query := r.URL.Query()
+	dataType := query.Get("type")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(`{"message": "DeleteWeatherData called with type=` + dataType + `"}`))
+	if err != nil {
+		return
+	}
+}
